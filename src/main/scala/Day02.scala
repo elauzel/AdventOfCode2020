c59b226ffb8d0e813c05ca3be0c2eb2160aeae00
jeremygilreath <elauzel@gmail.com>
2021-04-24 11:46:45 -0400

    finished day 2

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,39 +1,66 @@
 object Day02 {
-  case class PasswordAndPolicy(minOccurrances: Int,
-                          maxOccurrances: Int,
-                          characterOccurring: Char,
-                          password: String)
+  case class PasswordAndOccurrancePolicy(minOccurrances: Int,
+                                         maxOccurrances: Int,
+                                         character: Char,
+                                         password: String)
 
-  def passwordMeetsCompanyPolicy(passwordAndPolicy: PasswordAndPolicy): Boolean = {
-    val count = passwordAndPolicy.password.count(_ == passwordAndPolicy.characterOccurring)
+  case class PasswordAndPositionPolicy(index1: Int,
+                                       index2: Int,
+                                       character: Char,
+                                       password: String)
+
+  def passwordMeetsCompanyOccurrancePolicy(passwordAndPolicy: PasswordAndOccurrancePolicy): Boolean = {
+    val count = passwordAndPolicy.password.count(_ == passwordAndPolicy.character)
     (count >= passwordAndPolicy.minOccurrances) && (count <= passwordAndPolicy.maxOccurrances)
   }
 
-  def parsePasswordAndPolicy(line: String): PasswordAndPolicy = {
-    val lineParts = line.split(" ")
-    val frequencyPart = lineParts.head.split("-")
-    val min = frequencyPart.head.toInt
-    val max = frequencyPart.last.toInt
-    val character = lineParts(1).head
-    val password = lineParts.last
-    PasswordAndPolicy(min, max, character, password)
+  def passwordMeetsCompanyPositionPolicy(passwordAndPolicy: PasswordAndPositionPolicy): Boolean = {
+    val charAtIndex1 = passwordAndPolicy.password(passwordAndPolicy.index1 - 1) == passwordAndPolicy.character
+    val charAtIndex2 = passwordAndPolicy.password(passwordAndPolicy.index2 - 1) == passwordAndPolicy.character
+    charAtIndex1 ^ charAtIndex2
   }
 
-  def countPasswordsMeetingCompanyPolicy(lines: Vector[String]): Long =
+  def parsePasswordAndOccurrancePolicy(line: String): PasswordAndOccurrancePolicy = {
+    val info = parsePasswordAndPolicyInfo(line)
+    PasswordAndOccurrancePolicy(info._1, info._2, info._3, info._4)
+  }
+
+  def parsePasswordAndPositionPolicy(line: String): PasswordAndPositionPolicy = {
+    val info = parsePasswordAndPolicyInfo(line)
+    PasswordAndPositionPolicy(info._1, info._2, info._3, info._4)
+  }
+
+  private def parsePasswordAndPolicyInfo(line: String) = {
+    val lineParts = line.split(" ")
+    val frequencyPart = lineParts.head.split("-")
+    val number1 = frequencyPart.head.toInt
+    val number2 = frequencyPart.last.toInt
+    val character = lineParts(1).head
+    val password = lineParts.last
+    (number1, number2, character, password)
+  }
+
+  def countPasswordsMeetingOccurrancePolicy(lines: Vector[String]): Long =
     lines.count { line =>
-      val passwordAndPolicy = parsePasswordAndPolicy(line)
-      passwordMeetsCompanyPolicy(passwordAndPolicy)
+      val passwordAndPolicy = parsePasswordAndOccurrancePolicy(line)
+      passwordMeetsCompanyOccurrancePolicy(passwordAndPolicy)
+    }
+
+  def countPasswordsMeetingPositionPolicy(lines: Vector[String]): Long =
+    lines.count { line =>
+      val passwordAndPolicy = parsePasswordAndPositionPolicy(line)
+      passwordMeetsCompanyPositionPolicy(passwordAndPolicy)
     }
 
   def main(args: Array[String]): Unit =
     FileUtil.readResource("Day02.txt") match {
       case Left(exception) => throw exception
       case Right(lines) =>
-        // Part 1 - How many passwords are valid according to historical company policies?
-        val part1Answer = countPasswordsMeetingCompanyPolicy(lines)
+        // Part 1 - How many passwords meet company policies, using the two numbers as min/max character occurrances?
+        val part1Answer = countPasswordsMeetingOccurrancePolicy(lines)
         System.out.println(part1Answer)
-        // Part 2 - ???
-//        val part2Answer = ???
-//        System.out.println(part2Answer)
+        // Part 2 - How many passwords meet company policies, using the two numbers as the index where character must occur only once?
+        val part2Answer = countPasswordsMeetingPositionPolicy(lines)
+        System.out.println(part2Answer)
     }
 }
--- src/test/scala/Day02Spec.scala
+++ src/test/scala/Day02Spec.scala
@@ -1,33 +1,52 @@
-import Day02.PasswordAndPolicy
+import Day02.{PasswordAndOccurrancePolicy, PasswordAndPositionPolicy}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day02Spec extends AnyFlatSpec with Matchers {
-  "password meets company policy" should "return true if the specified char occurs min ≤ n ≤ max times in the password" in {
-    Day02.passwordMeetsCompanyPolicy(PasswordAndPolicy(1, 3, 'a', "abc")) should be(true)
-    Day02.passwordMeetsCompanyPolicy(PasswordAndPolicy(1, 3, 'a', "aac")) should be(true)
-    Day02.passwordMeetsCompanyPolicy(PasswordAndPolicy(1, 3, 'a', "aaa")) should be(true)
+  "password meets company occurrance policy" should "return true if the specified char occurs min ≤ n ≤ max times in the password" in {
+    Day02.passwordMeetsCompanyOccurrancePolicy(PasswordAndOccurrancePolicy(1, 3, 'a', "abc")) should be(true)
+    Day02.passwordMeetsCompanyOccurrancePolicy(PasswordAndOccurrancePolicy(1, 3, 'a', "aac")) should be(true)
+    Day02.passwordMeetsCompanyOccurrancePolicy(PasswordAndOccurrancePolicy(1, 3, 'a', "aaa")) should be(true)
   }
 
-  "password meets company policy" should "return false if the specified char occurs < min or > max times in the password" in {
-    Day02.passwordMeetsCompanyPolicy(PasswordAndPolicy(1, 3, 'a', "bc")) should be(false)
-    Day02.passwordMeetsCompanyPolicy(PasswordAndPolicy(1, 3, 'a', "aaaa")) should be(false)
+  "password meets company occurrance policy" should "return false if the specified char occurs < min or > max times in the password" in {
+    Day02.passwordMeetsCompanyOccurrancePolicy(PasswordAndOccurrancePolicy(1, 3, 'a', "bc")) should be(false)
+    Day02.passwordMeetsCompanyOccurrancePolicy(PasswordAndOccurrancePolicy(1, 3, 'a', "aaaa")) should be(false)
   }
 
-  "parse password and policy" should "parse a character, the minimum and maximum times it can occur, and a password, from a line" in {
-    Day02.parsePasswordAndPolicy("1-3 a: abcde") should be(PasswordAndPolicy(1, 3, 'a', "abcde"))
-    Day02.parsePasswordAndPolicy("1-3 b: cdefg") should be(PasswordAndPolicy(1, 3, 'b', "cdefg"))
-    Day02.parsePasswordAndPolicy("2-9 c: ccccccccc") should be(PasswordAndPolicy(2, 9, 'c', "ccccccccc"))
+  "parse password and occurrance policy" should "parse a character, the minimum and maximum times it can occur, and a password, from a line" in {
+    Day02.parsePasswordAndOccurrancePolicy("1-3 a: abcde") should be(PasswordAndOccurrancePolicy(1, 3, 'a', "abcde"))
+    Day02.parsePasswordAndOccurrancePolicy("1-3 b: cdefg") should be(PasswordAndOccurrancePolicy(1, 3, 'b', "cdefg"))
+    Day02.parsePasswordAndOccurrancePolicy("2-9 c: ccccccccc") should be(PasswordAndOccurrancePolicy(2, 9, 'c', "ccccccccc"))
+  }
+
+  "password meets company position policy" should "return true if the specified char occurs exactly once at each specified index in the password" in {
+    Day02.passwordMeetsCompanyPositionPolicy(PasswordAndPositionPolicy(1, 3, 'a', "abc")) should be(true)
+    Day02.passwordMeetsCompanyPositionPolicy(PasswordAndPositionPolicy(1, 3, 'a', "cba")) should be(true)
+  }
+
+  "password meets company position policy" should "return true if the specified char does not occurs exactly once at each specified index in the password" in {
+    Day02.passwordMeetsCompanyPositionPolicy(PasswordAndPositionPolicy(1, 3, 'a', "baca")) should be(false)
+    Day02.passwordMeetsCompanyPositionPolicy(PasswordAndPositionPolicy(1, 3, 'a', "abac")) should be(false)
+  }
+
+  "parse password and position policy" should "parse a character, the first and second index at which it can occur, and a password, from a line" in {
+    Day02.parsePasswordAndPositionPolicy("1-3 a: abcde") should be(PasswordAndPositionPolicy(1, 3, 'a', "abcde"))
+    Day02.parsePasswordAndPositionPolicy("1-3 b: cdefg") should be(PasswordAndPositionPolicy(1, 3, 'b', "cdefg"))
+    Day02.parsePasswordAndPositionPolicy("2-9 c: ccccccccc") should be(PasswordAndPositionPolicy(2, 9, 'c', "ccccccccc"))
   }
 
   // Part 1
 
-  "count passwords meeting company policy" should "count how many lines have passwords that meet the contained company policy" in {
+  "count passwords meeting company occurrance policy" should "count how many lines have passwords that meet the contained company policy" in {
     val lines = Vector("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc")
-    Day02.countPasswordsMeetingCompanyPolicy(lines) should be(2)
+    Day02.countPasswordsMeetingOccurrancePolicy(lines) should be(2)
   }
 
   // Part 2
 
-  // TODO
+  "count passwords meeting company position policy" should "count how many lines have passwords that meet the contained company policy" in {
+    val lines = Vector("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc")
+    Day02.countPasswordsMeetingPositionPolicy(lines) should be(1)
+  }
 }
