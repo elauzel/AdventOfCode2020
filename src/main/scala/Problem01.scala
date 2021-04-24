d618eb8c044969998e91664906ef21b4a3482942
jeremygilreath <elauzel@gmail.com>
2021-04-24 10:46:54 -0400

    finished part 2

--- src/main/scala/Problem01.scala
+++ src/main/scala/Problem01.scala
@@ -1,32 +1,49 @@
 import scala.annotation.tailrec
 
 object Problem01 {
-  def findProductOfTwoSummedNumbers(desiredSum: Int, numbers: Vector[Int]): Either[Throwable, Long] = {
-    @tailrec
-    def go(desiredSum: Int, augend: Int, addendsToCheck: Vector[Int]): Either[Throwable, Long] = {
-      addendsToCheck
-        .find(_ + augend == desiredSum) match {
-        case Some(addend) =>
-          Right(augend * addend)
-        case None =>
-          if (addendsToCheck.size < 2)
-            Left(new RuntimeException("no remaining numbers to check"))
-          else
-            go(desiredSum, addendsToCheck.head, addendsToCheck.tail)
-      }
-    }
+  def findProductOfTwoSummedNumbers(desiredSum: Long, numbers: Vector[Long]): Either[Throwable, Long] =
+    if (numbers.size < 2)
+      Left(new RuntimeException(s"no two numbers found with desired sum $desiredSum"))
+    else {
+      @tailrec
+      def go(desiredSum: Long, augend: Long, addendsToCheck: Vector[Long]): Either[Throwable, Long] =
+        addendsToCheck.find(_ + augend == desiredSum) match {
+          case Some(addend) =>
+            Right(augend * addend)
+          case None =>
+            if (addendsToCheck.size < 2)
+              Left(new RuntimeException(s"no two numbers found with desired sum $desiredSum"))
+            else
+              go(desiredSum, addendsToCheck.head, addendsToCheck.tail)
+        }
 
-    if (numbers.size < 2) Left(new RuntimeException("need at least two numbers to sum"))
-    else go(desiredSum, numbers.head, numbers.tail)
+      go(desiredSum, numbers.head, numbers.tail)
+  }
+
+  @tailrec
+  def findProductOfThreeSummedNumbers(desiredSum: Long, numbers: Vector[Long]): Either[Throwable, Long] =
+    if (numbers.length < 3)
+      Left(new RuntimeException(s"no three numbers found with desired sum $desiredSum"))
+    else {
+    val number = numbers.head
+    findProductOfTwoSummedNumbers(desiredSum - number, numbers.tail) match {
+      case Left(_) =>
+        findProductOfThreeSummedNumbers(desiredSum, numbers.tail)
+      case Right(product) =>
+        Right(product * number)
+    }
   }
 
   def main(args: Array[String]): Unit =
-  // Part 1 - Find the two numbers that sum to 2020 then multiply them together
     FileUtil.readResource("Problem01.txt") match {
       case Left(exception) => throw exception
       case Right(lines) =>
-        val numbers = lines.map(_.toInt)
-        val answer = findProductOfTwoSummedNumbers(2020, numbers)
-        System.out.println(answer)
+        val numbers = lines.map(_.toLong)
+        // Part 1 - Find the two numbers that sum to 2020 then multiply them together
+        val part1Answer = findProductOfTwoSummedNumbers(2020, numbers)
+        System.out.println(part1Answer)
+        // Part 2 - Find the three numbers that sum to 2020 then multiply them together
+        val part2Answer = findProductOfThreeSummedNumbers(2020, numbers)
+        System.out.println(part2Answer)
     }
 }
--- src/test/scala/Problem01Spec.scala
+++ src/test/scala/Problem01Spec.scala
@@ -4,23 +4,37 @@ import org.scalatest.matchers.should.Matchers
 class Problem01Spec extends AnyFlatSpec with Matchers {
   // Part 1
 
-  "Problem 01" should "find the product of the two entries that sum to a desired number" in {
-    val numbers = Vector(1721, 979, 366, 299, 675, 1456)
+  "find product of two summed numbers" should "find the product of the two entries that sum to a desired number" in {
+    val numbers = Vector[Long](1721, 979, 366, 299, 675, 1456)
     Problem01.findProductOfTwoSummedNumbers(2020, numbers) should be(Right(1721 * 299))
   }
 
-  "Problem 01" should "find no product if there are fewer than two numbers" in {
-    val numbers = Vector(2020)
+  "find product of two summed numbers" should "find no product if there are fewer than two numbers" in {
+    val numbers = Vector[Long](2020)
     Problem01.findProductOfTwoSummedNumbers(2020, numbers).isLeft should be(true)
-    Problem01.findProductOfTwoSummedNumbers(2020, Vector.empty[Int]).isLeft should be(true)
+    Problem01.findProductOfTwoSummedNumbers(2020, Vector.empty[Long]).isLeft should be(true)
   }
 
-  "Problem 01" should "find no product if there are no two entries that sum to a desired number" in {
-    val numbers = Vector(1, 2, 3)
+  "find product of two summed numbers" should "find no product if there are no two entries that sum to a desired number" in {
+    val numbers = Vector[Long](1, 2, 3)
     Problem01.findProductOfTwoSummedNumbers(6, numbers).isLeft should be(true)
   }
 
   // Part 2
 
-  // TODO
+  "find product of three summed numbers" should "find the product of the three entries that sum to a desired number" in {
+    val numbers = Vector[Long](1721, 979, 366, 299, 675, 1456)
+    Problem01.findProductOfThreeSummedNumbers(2020, numbers) should be(Right(979 * 366 * 675))
+  }
+
+  "find product of three summed numbers" should "find no product if there are fewer than three numbers" in {
+    val numbers = Vector[Long](1, 2019)
+    Problem01.findProductOfThreeSummedNumbers(2020, numbers).isLeft should be(true)
+    Problem01.findProductOfThreeSummedNumbers(2020, Vector.empty[Long]).isLeft should be(true)
+  }
+
+  "find product of three summed numbers" should "find no product if there are no three entries that sum to a desired number" in {
+    val numbers = Vector[Long](1, 2, 3)
+    Problem01.findProductOfThreeSummedNumbers(7, numbers).isLeft should be(true)
+  }
 }
