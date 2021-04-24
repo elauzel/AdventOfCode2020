d2518982fbcd598b6a5c054de02870c33032a7d9
jeremygilreath <elauzel@gmail.com>
2021-04-24 10:01:39 -0400

    refactored and made tail-recursive

--- src/main/scala/Problem01.scala
+++ src/main/scala/Problem01.scala
@@ -1,20 +1,27 @@
+import scala.annotation.tailrec
+
 object Problem01 {
-  def findProductOfTwoSummedNumbers(desiredSum: Int, numbers: Vector[Int]): Option[Int] = {
-    def go(desiredSum: Int, augend: Int, addendsToCheck: Vector[Int]): Option[Int] = { // TODO make tail-recursive
+  def findProductOfTwoSummedNumbers(desiredSum: Int, numbers: Vector[Int]): Either[Throwable, Long] = {
+    @tailrec
+    def go(desiredSum: Int, augend: Int, addendsToCheck: Vector[Int]): Either[Throwable, Long] = {
       addendsToCheck
-        .find(_ + augend == desiredSum)
-        .map(_ * augend)
-        .orElse {
-          if (addendsToCheck.size < 2) None // no remaining numbers to check
-          else go(desiredSum, addendsToCheck.head, addendsToCheck.tail)
-        }
+        .find(_ + augend == desiredSum) match {
+        case Some(addend) =>
+          Right(augend * addend)
+        case None =>
+          if (addendsToCheck.size < 2)
+            Left(new RuntimeException("no remaining numbers to check"))
+          else
+            go(desiredSum, addendsToCheck.head, addendsToCheck.tail)
+      }
     }
-    if (numbers.size < 2) None // need at least two numbers to sum
+
+    if (numbers.size < 2) Left(new RuntimeException("need at least two numbers to sum"))
     else go(desiredSum, numbers.head, numbers.tail)
   }
 
   def main(args: Array[String]): Unit =
-    // Find the two numbers that sum to 2020 then multiply them together
+  // Part 1 - Find the two numbers that sum to 2020 then multiply them together
     FileUtil.readResource("Problem01.txt") match {
       case Left(exception) => throw exception
       case Right(lines) =>
