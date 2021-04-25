1abb1d157e3a2bf2dccf2c785b45d5f0c8265132
jeremygilreath <elauzel@gmail.com>
2021-04-25 18:21:04 -0400

    finished day 6

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -1,16 +1,21 @@
 object Day06 {
-  def countQuestionsAnsweredPerGroup(group: Vector[String]): Int =
-    group.mkString("").distinct.size
+  def countQuestionsAffirmedByAnyone(group: Vector[String]): Int =
+    group.mkString("").distinct.length
+
+  def countQuestionsAffirmedByEveryone(group: Vector[String]): Int =
+    group.mkString("")
+      .distinct
+      .count(c => group.forall(_.contains(c)))
 
   def main(args: Array[String]): Unit =
     FileUtil.readResourceAsGroupedLines("Day06.txt") match {
       case Left(exception) => throw exception
       case Right(lines) =>
-        // Part 1 - For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts
-        val part1Answer = lines.map(countQuestionsAnsweredPerGroup).sum
+        // Part 1 - For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+        val part1Answer = lines.map(countQuestionsAffirmedByAnyone).sum
         System.out.println(part1Answer)
-      // Part 2 - ???
-      //        val part2Answer = ???
-      //        System.out.println(part2Answer)
+        // Part 2 - For each group, count the number of questions to which *everyone* answered "yes". What is the sum of those counts?
+        val part2Answer = lines.map(countQuestionsAffirmedByEveryone).sum
+        System.out.println(part2Answer)
     }
 }
--- src/test/scala/Day06Spec.scala
+++ src/test/scala/Day06Spec.scala
@@ -2,11 +2,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day06Spec extends AnyFlatSpec with Matchers {
-  "count questions answered per group" should "count how many distinct questions were answered yes by the group" in {
-    Day06.countQuestionsAnsweredPerGroup(Vector("abc")) should be(3)
-    Day06.countQuestionsAnsweredPerGroup(Vector("a", "b", "c")) should be(3)
-    Day06.countQuestionsAnsweredPerGroup(Vector("ab", "ac")) should be(3)
-    Day06.countQuestionsAnsweredPerGroup(Vector("a", "a", "a", "a")) should be(1)
-    Day06.countQuestionsAnsweredPerGroup(Vector("b")) should be(1)
+  // Part 1
+  "count questions affirmed by anyone" should "count how many distinct questions were answered yes by anyone in the group" in {
+    Day06.countQuestionsAffirmedByAnyone(Vector("abc")) should be(3)
+    Day06.countQuestionsAffirmedByAnyone(Vector("a", "b", "c")) should be(3)
+    Day06.countQuestionsAffirmedByAnyone(Vector("ab", "ac")) should be(3)
+    Day06.countQuestionsAffirmedByAnyone(Vector("a", "a", "a", "a")) should be(1)
+    Day06.countQuestionsAffirmedByAnyone(Vector("b")) should be(1)
+  }
+
+  // Part 2
+
+  "count questions affirmed by everyone" should "count how many distinct questions were answered yes by everyone in the group" in {
+    Day06.countQuestionsAffirmedByEveryone(Vector("abc")) should be(3)
+    Day06.countQuestionsAffirmedByEveryone(Vector("a", "b", "c")) should be(0)
+    Day06.countQuestionsAffirmedByEveryone(Vector("ab", "ac")) should be(1)
+    Day06.countQuestionsAffirmedByEveryone(Vector("a", "a", "a", "a")) should be(1)
+    Day06.countQuestionsAffirmedByEveryone(Vector("b")) should be(1)
   }
 }
