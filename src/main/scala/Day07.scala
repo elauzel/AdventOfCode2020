94a3f8860a2131154476a399cba594704e61d2c0
jeremygilreath <elauzel@gmail.com>
2021-04-25 20:24:08 -0400

    finished day 7 part 1

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -1,31 +1,28 @@
 object Day07 {
   type BagColor = String
 
-  case class Rule(bagColor: BagColor, allowedContents: Map[Int, BagColor])
+  case class Rule(bagColor: BagColor, allowedContents: Map[BagColor, Int])
 
   def parseRule(line: String): Rule =
     line match {
       case s"$bagColor bags contain no other bags." =>
-        Rule(bagColor, Map.empty[Int, BagColor])
+        Rule(bagColor, Map.empty[BagColor, Int])
       case s"$bagColor bags contain $possibleContents." =>
         val allowedContents = possibleContents.split(',')
           .map(_.trim match {
             case s"$number $innerBagName bag$_" =>
-              number.toInt -> innerBagName
+              innerBagName -> number.toInt
           }).toMap
         Rule(bagColor, allowedContents)
       case _ =>
         throw new RuntimeException("line formatted incorrectly for rule parsing")
     }
 
-  def findPossibleBagsToHold(bagColor: BagColor, rules: Set[Rule]): Set[BagColor] =
-    findBags(bagColor, rules)
-
-  private def findBags(bagColor: BagColor, rules: Set[Rule]): Set[BagColor] = {
+  def findPossibleBagsToHold(bagColor: BagColor, rules: Set[Rule]): Set[BagColor] = {
     val newBags = rules
-      .filter(_.allowedContents.values.exists(_ == bagColor))
+      .filter(_.allowedContents.keys.exists(_ == bagColor))
       .map(_.bagColor)
-    newBags ++ newBags.flatMap(color => findBags(color, rules))
+    newBags ++ newBags.flatMap(color => findPossibleBagsToHold(color, rules))
   }
 
   def main(args: Array[String]): Unit =
--- src/test/scala/Day07Spec.scala
+++ src/test/scala/Day07Spec.scala
@@ -4,31 +4,44 @@ import org.scalatest.matchers.should.Matchers
 
 class Day07Spec extends AnyFlatSpec with Matchers {
   "parse rule" should "parse a bag that contains no other bags" in {
-    val rule = Rule("striped lavender", Map.empty[Int, String])
+    val rule = Rule("striped lavender", Map.empty[String, Int])
     Day07.parseRule("striped lavender bags contain no other bags.") should be(rule)
   }
 
   "parse rule" should "parse a bag that contains one other bag" in {
-    val rule = Rule("striped lavender", Map(1 -> "pale lavender"))
+    val rule = Rule("striped lavender", Map("pale lavender" -> 1))
     Day07.parseRule("striped lavender bags contain 1 pale lavender bag.") should be(rule)
   }
 
   "parse rule" should "parse a bag that contains several other bags" in {
-    val rule = Rule("striped lavender", Map(3 -> "pale lavender", 4 -> "muted cyan"))
+    val rule = Rule("striped lavender", Map("pale lavender" -> 3, "muted cyan" -> 4))
     Day07.parseRule("striped lavender bags contain 3 pale lavender bags, 4 muted cyan bags.") should be(rule)
   }
 
   // Part 1
 
-  "find possible bags to hold" should "find all bag colors which can directly or indirectly hold another bag color" in {
+  "find possible bags to hold" should "find all bag colors which can directly hold another bag color" in {
     val rules = Set(
-      Rule("red", Map.empty[Int, String]),
-      Rule("orange", Map(1 -> "red")),
-      Rule("yellow", Map(1 -> "red", 1 -> "orange")),
-      Rule("green", Map(1 -> "red", 1 -> "orange", 1 -> "yellow")),
-      Rule("blue", Map(1 -> "orange", 1 -> "yellow", 1 -> "green")),
-      Rule("indigo", Map(1 -> "yellow", 1 -> "green", 1 -> "blue")),
-      Rule("violet", Map(1 -> "green", 1 -> "blue", 1 -> "indigo")),
+      Rule("orange", Map("red" -> 1, "etc" -> 1)),
+      Rule("yellow", Map("red" -> 1, "etc" -> 1)),
+      Rule("green", Map("red" -> 1, "etc" -> 1)),
+      Rule("blue", Map("red" -> 1, "etc" -> 1)),
+      Rule("indigo", Map("red" -> 1, "etc" -> 1)),
+      Rule("violet", Map("red" -> 1, "etc" -> 1))
+    )
+
+    Day07.findPossibleBagsToHold("red", rules) should be(Set("orange", "yellow", "green", "blue", "indigo", "violet"))
+  }
+
+  "find possible bags to hold" should "find all bag colors which can indirectly hold another bag color" in {
+    val rules = Set(
+      Rule("red", Map.empty[String, Int]),
+      Rule("orange", Map("red" -> 1)),
+      Rule("yellow", Map("orange" -> 1)),
+      Rule("green", Map("yellow" -> 1)),
+      Rule("blue", Map("green" -> 1)),
+      Rule("indigo", Map("blue" -> 1)),
+      Rule("violet", Map("indigo" -> 1)),
     )
 
     Day07.findPossibleBagsToHold("red", rules) should be(Set("orange", "yellow", "green", "blue", "indigo", "violet"))
